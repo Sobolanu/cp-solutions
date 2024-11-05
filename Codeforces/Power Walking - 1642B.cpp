10b99a02952dcd04350d9ee08129e29294623ab8
Sobolanu <Sobolanu303@gmail.com>
2024-11-05 20:18:25 +0100

    Added 1642B.

---
+++ Codeforces/Power Walking - 1642B.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <map>
+
+/*
+yoinked from editorial, thought of using a cnt array however
+i forgot std::map exists and is way better for this kind of purpose.
+10^9 ints is 4GB, which is definitely outside of mem limit
+*/
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false); std::cin.tie(0);
+
+    int t{};
+    std::cin >> t;
+
+    while(t--)
+    {
+        int n{}, cnt{};
+        std::cin >> n;
+
+        std::map<int, int> d{};
+        for(int i = 0; i < n; i++)
+        {
+            int x{};
+            std::cin >> x;
+            d[x]++;
+        }
+
+        for(auto x : d) cnt++;
+
+        for(int k = 1; k <= n; k++) // this type of thing was on my mind!
+            std::cout << std::max(k, cnt) << " ";
+
+        std::cout << '\n';
+    }
+}
