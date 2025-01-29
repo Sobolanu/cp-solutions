75b30323e2d3899edef08c3fb035688e1fd41f80
Sobolanu <Sobolanu303@gmail.com>
2025-01-29 19:22:56 +0100

    Added 2060D.

---
+++ Codeforces/Subtract Min Sort - 2060D.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(), x.end()
+#define int long long
+
+using namespace std;
+
+/*
+my idea was right, i just didn't know that
+if the array is sorted at the n-th operation then it will
+be sorted once you finish all operations
+*/
+main()
+{
+    std::ios::sync_with_stdio(false); std::cin.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+		cin >> n;
+
+		vector<int> v(n);
+		for(auto& x : v) cin >> x;
+
+		for(int i = 0; i < n - 1; i++)
+		{
+			int mn = min(v[i], v[i+1]);
+			v[i] -= mn;
+			v[i+1] -= mn;
+
+			if(v[i] < 0 || v[i+1] < 0)
+				break;
+		}
+
+		if(is_sorted(all(v)))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+}
